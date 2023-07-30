eeefd1e406e103e605c53b055edd0563c7dd29d3
MarquessMateus <tsjesus21@outlook.com>
2023-07-30 16:56:36 -0300

    Primeiro Hello Word do crud

--- Python/CRUD/settings.py
+++ Python/CRUD/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$67a^a3ag9v%7*gxlhh65q^0+q7o%98+(v%^%1*=-jk!ug*597'
+SECRET_KEY = 'django-insecure-10_7$wr@izjv2_$7_b&_ke_%^qi=-b-n*9=1418)-tw2+k4j*a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- Python/app/views.py
+++ Python/app/views.py
@@ -1,18 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
 def home(request):
-    return HttpResponse(
-        '<h1> Hello word do CRUD</h1>'
-
-    )
-
-def login(request):
-    return HttpResponse(
-
-        'Tela de Login'
-
-
-    )
+    return HttpResponse( 'Hello Word')
